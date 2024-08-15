b9bf1ba8b92b57aa4afd336d317276c099d39a1c
Quentin Berthet <quentin@berthet.ch>
2024-08-15 17:14:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Synchroniser les commandes d'application avec le bot
+@bot.event
+async def on_ready():
+    await bot.tree.sync()  # Synchronise les commandes
+    print(f'Bot connecté en tant que {bot.user}!')
+
+# Commande traditionnelle
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Hello, {ctx.author}')
+    await ctx.send(f'Hello, {ctx.author}!')
 
+# Commande slash
+@bot.tree.command(name="test", description="a test")
+async def test(interaction: discord.Interaction):
+    await interaction.response.send_message("This is a test!")
 
 token = os.getenv('BOT_TOKEN')
 bot.run(token)
