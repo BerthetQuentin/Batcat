5fdaefee32ae4ed50e81392024154980c5705a86
Berthet Quentin <quentin@berthet.ch>
2024-09-23 22:38:18 +0200

    add / command

--- cogs/test.py
+++ cogs/test.py
@@ -1,13 +1,14 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='test')
-    async def test(self, ctx):
-        await ctx.send('test')
+    @app_commands.command(name='test', description='This is a test command')
+    async def test_command(self, interaction: discord.Interaction):
+        await interaction.response.send_message('Test command executed from slash command!')
 
 async def setup(bot):
     await bot.add_cog(Test(bot))
